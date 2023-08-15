352c9e247d8d83a25ea4208b841442911956f77f
Semyon Zasov <semenmaslina@gmail.com>
2023-08-15 10:01:09 +0300

    feat: created template method for reusing product.

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -8,17 +8,15 @@ class Product {
       .map((data) => {
         document
           .querySelector(".product-wrapper")
-          .insertAdjacentHTML("beforeend", template(data));
+          .insertAdjacentHTML("beforeend", this.template(data));
       });
-  }
 
-  renderSoldout(products) {
     products
       .filter(({ availability }) => !availability)
       .map((data) => {
         document
           .querySelector(".product-wrapper-soldout")
-          .insertAdjacentHTML("beforeend", template(data));
+          .insertAdjacentHTML("beforeend", this.template(data));
       });
   }
 
@@ -58,44 +56,44 @@ class Product {
       }
     });
   }
-}
 
-export const template = (data) => {
-  return `
-  <li>
-   <article id="product-${data.id}" class="product ${
-     data.availability ? `` : `product--soldout`
-   }">
-  
-   ${data.availability ? checkbox.template(data.id) : ``}
- 
-   <img class="product__img" src=${data.img} />
- 
-   <div class="product__text">
-     <h2 class="product__text-title">${data.title}</h2>
-     <div class="product__text-description">
-       ${data.color ? `<span>Цвет: ${data.color}</span>` : ``}
-       ${data.size ? `<span>Размер: ${data.size}</span>` : ``}
-     </div>
-     <div class="product__text-company">
-       <div>${data.IP}</div>
-       <div>${data.OOO} <img src="./src/icons/info.svg"></div>
-     </div>
-   </div>
- 
-   ${counter.template(1, data.count, data.id, data.availability)}
- 
-   ${
-     data.availability
-       ? `<div class="product__price">
-           <div class="product__price-total">${data.discount} <span>сом</span></div>
-           <div class="product__price-discount">${data.price} <span>сом</span></div>
-         </div>`
-       : ``
-   }
-   </article>
-  </li>
-   `;
-};
+  template(data) {
+    return `
+      <li>
+      <article id="product-${data.id}" class="product ${
+        data.availability ? `` : `product--soldout`
+      }">
+      
+      ${data.availability ? checkbox.template(data.id) : ``}
+    
+      <img class="product__img" src=${data.img} />
+    
+      <div class="product__text">
+        <h2 class="product__text-title">${data.title}</h2>
+        <div class="product__text-description">
+          ${data.color ? `<span>Цвет: ${data.color}</span>` : ``}
+          ${data.size ? `<span>Размер: ${data.size}</span>` : ``}
+        </div>
+        <div class="product__text-company">
+          <div>${data.IP}</div>
+          <div>${data.OOO} <img src="./src/icons/info.svg"></div>
+        </div>
+      </div>
+    
+      ${counter.template(1, data.count, data.id, data.availability)}
+    
+      ${
+        data.availability
+          ? `<div class="product__price">
+              <div class="product__price-total">${data.discount} <span>сом</span></div>
+              <div class="product__price-discount">${data.price} <span>сом</span></div>
+            </div>`
+          : ``
+      }
+      </article>
+      </li>
+    `;
+  }
+}
 
 export const product = new Product();
